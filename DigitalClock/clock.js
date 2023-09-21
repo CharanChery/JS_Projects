07723d1694d85239ce50b0822d1eab54f3fa8f59
CharanChery <95143627+CharanChery@users.noreply.github.com>
2023-09-21 18:37:53 +0530

    DC

---
+++ DigitalClock/clock.js
@@ -0,0 +1,19 @@
+/* eslint-disable quotes */
+/* eslint-disable semi */
+const hrs = document.getElementById("hrs");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+const date = document.getElementById("dt");
+const day = document.getElementById("dy");
+const month = document.getElementById("mth");
+const year = document.getElementById("yyyy");
+setInterval(() => {
+  const clock = new Date();
+  hrs.innerHTML = (clock.getHours() < 10 ? "0" : "") + clock.getHours();
+  min.innerHTML = (clock.getMinutes() < 10 ? "0" : "") + clock.getMinutes();
+  sec.innerHTML = (clock.getSeconds() < 10 ? "0" : "") + clock.getSeconds();
+  date.innerHTML = clock.getDate();
+  day.innerHTML = clock.getDay();
+  month.innerHTML = clock.getMonth();
+  year.innerHTML = clock.getFullYear();
+}, 1000);
